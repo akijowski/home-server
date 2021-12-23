ab6088733ea67099e3bd44aded1cb5b993a62ae0
Adam Kijowski <agkijow@gmail.com>
2021-12-22 19:58:09 -0700

    switch to helm chart for prom stack.  Add some resources

---
+++ kube/kube-prometheus/helm.tf
@@ -0,0 +1,16 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "this" {
+    name = "kube-prometheus"
+    repository = "https://prometheus-community.github.io/helm-charts"
+    chart = "kube-prometheus-stack"
+
+    namespace = "monitoring"
+    create_namespace = true
+
+    values = ["${file("values.yml")}"]
+}
